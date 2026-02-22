0df8484426168e928c67d268fe1945248e8b510b
prajwal9680 <prajwal9680@gmail.com>
2026-02-22 14:46:19 +0530

    ram modification

--- finetune/dataset.py
+++ finetune/dataset.py
@@ -3,13 +3,18 @@ import tiktoken
 import torch
 
 def build_dataset(output_path):
-    print("Building dataset...")
+    print("Building dataset (streaming)...")
 
     dataset = load_dataset("openwebtext", split="train[:5%]")
-    all_text = "\n".join(dataset["text"])
 
     enc = tiktoken.get_encoding("gpt2")
-    tokens = torch.tensor(enc.encode(all_text), dtype=torch.long)
+
+    tokens = []
+
+    for example in dataset:
+        tokens.extend(enc.encode(example["text"]))
+
+    tokens = torch.tensor(tokens, dtype=torch.long)
 
     torch.save(tokens, output_path)
 
